ebfe2312e25ecf15890e24113ab9c384b0a46da4
egutter <egutter@10pines.com>
2023-04-24 17:13:49 -0300

    Add missing field to Shaolin struct

--- lib/blade_fury/warrior/shaolin.ex
+++ lib/blade_fury/warrior/shaolin.ex
@@ -1,7 +1,7 @@
 defmodule BladeFury.Warrior.Shaolin do
   import BladeFury.Weapon, only: [increase_shaolin_power: 2]
 
-  defstruct strength: 10
+  defstruct [:weapon, strength: 10]
 
   defimpl BladeFury.Warrior.Power do
     def power(warrior) do
